0c365b35fdcaebe4079e38a107e1c38bb3101975
unknown <osokuso@hotmail.com>
2015-04-03 11:17:58 +0200

    Creación insertAfter

--- Uma/src/main/java/doublylinkedlist/DoublyLinkedList.java
+++ Uma/src/main/java/doublylinkedlist/DoublyLinkedList.java
@@ -1,5 +1,22 @@
 package doublylinkedlist;
-
+import java.util.*;
 public class DoublyLinkedList {
+	// points to first node of list
+		DNode firstNode;
+		// points to last node of list
+	    DNode lastNode;
 
+	    
+	    public void insertAfter(List<DNode> list,DNode node,DNode newNode ){
+	    	newNode.previousNode=node;
+	    	newNode.nextNode=node.nextNode;
+	    		if(newNode.nextNode==null){
+	    			lastNode=newNode;
+	    			node.nextNode=newNode;
+	    		}else{
+	    			node.nextNode.previousNode=newNode;
+	    			node.nextNode=newNode;
+	    		}
+	    		list.add(newNode);
+	    }
 }
